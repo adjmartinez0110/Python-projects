7a4ced39f188bcab95ca90fb10a51347f6c11132
adjmartinez0110 <87952925+adjmartinez0110@users.noreply.github.com>
2023-02-11 14:19:01 -0600

    Add files via upload

---
+++ for-loop-intro.py
@@ -0,0 +1,10 @@
+#for loops 
+"""
+print("Let's play hide and seek. I count to 30 and you go hide.")
+print()
+
+for i in range(1, 31):
+  print(str(i) + " Mississippi")
+  
+print("Ready or not, here I come!")
+  """
